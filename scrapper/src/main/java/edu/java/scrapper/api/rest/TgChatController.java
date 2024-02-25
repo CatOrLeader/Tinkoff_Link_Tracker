84c79719a22a19388dc0c7ea643dec7984a9faf3
Artur <arturmuxutdinov@gmail.com>
2024-02-25 14:23:10 +0300

    feat(scrapper): structure&openapi
    migrate to one more nested folder; realize the API by provided spec; tested

--- scrapper/src/main/java/edu/java/scrapper/api/rest/TgChatController.java
+++ scrapper/src/main/java/edu/java/scrapper/api/rest/TgChatController.java
@@ -1,6 +1,5 @@
 package edu.java.scrapper.api.rest;
 
-import jakarta.validation.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
