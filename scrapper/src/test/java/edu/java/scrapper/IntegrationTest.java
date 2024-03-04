bdb1b353b9cb23b15c26bcf00204d35ed2551032
Artur <arturmuxutdinov@gmail.com>
2024-03-04 04:03:28 +0300

    impl(impl): liquibase&integration test

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,14 +1,31 @@
 package edu.java.scrapper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.OutputStreamWriter;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public abstract class IntegrationTest {
-    public static PostgreSQLContainer<?> POSTGRES;
+abstract class IntegrationTest {
+    static final PostgreSQLContainer<?> POSTGRES;
 
     static {
         POSTGRES = new PostgreSQLContainer<>("postgres:15")
@@ -20,14 +37,35 @@ public abstract class IntegrationTest {
         runMigrations(POSTGRES);
     }
 
-    private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+    private static void runMigrations(@NotNull JdbcDatabaseContainer<?> c) {
+        try (Connection connection = DriverManager.getConnection(c.getJdbcUrl(), c.getUsername(), c.getPassword())) {
+            Database database =
+                DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            var dir = new File("").toPath().toAbsolutePath().getParent().resolve(Path.of("db"));
+            Liquibase liquibase = new Liquibase(
+                Path.of("migrations", "db.master-changelog.xml").toString(),
+                new DirectoryResourceAccessor(dir),
+                database
+            );
+
+            liquibase.update(new Contexts(), new OutputStreamWriter(System.out));
+        } catch (SQLException | LiquibaseException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DynamicPropertySource
-    static void jdbcProperties(DynamicPropertyRegistry registry) {
+    static void jdbcProperties(@NotNull DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
+
+class SimpleStatusTest extends IntegrationTest {
+    @Test
+    void givenNothing_whenContainerIsStarted_thenRunningAndShining() {
+        assertThat(POSTGRES.isRunning()).isTrue();
     }
 }
