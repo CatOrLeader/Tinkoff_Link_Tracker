8c5ed06f7547aff33092bc0edacde3d294837143
Artur <arturmuxutdinov@gmail.com>
2024-03-10 22:11:09 +0300

    impl(github-api): deep polling
    Adjust github client to make wider amount of possible sources to check

--- scrapper/src/test/java/edu/java/scrapper/github/service/EventServiceTest.java
+++ scrapper/src/test/java/edu/java/scrapper/github/service/EventServiceTest.java
@@ -1,24 +1,18 @@
 package edu.java.scrapper.github.service;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import edu.java.scrapper.github.model.PullResponse;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.OffsetDateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class EventServiceTest {
