3ddeb72b3964a95d0540c29205a02ec8c6077bb0
Artur <arturmuxutdinov@gmail.com>
2024-03-17 22:06:58 +0300

    impl(postgres): liquibase
    add the necessary columns in the DB to the further refactoring process

--- db/migrations/v.0.0.2/17-03-2024-chat-rename-user-state-column.sql
+++ db/migrations/v.0.0.2/17-03-2024-chat-rename-user-state-column.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset catorleader:6 dbms:postgresql
+-- changeset catorleader:16 dbms:postgresql
 ALTER TABLE tg_chat
 RENAME COLUMN dialogstate TO dialog_state;
 -- rollback ALTER TABLE tg_chat RENAME COLUMN dialog_state TO dialogstate;
